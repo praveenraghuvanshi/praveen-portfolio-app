7cc5f8553c0b291771c4f23a5afe41e4624ec696
praveen raghuvanshi <praveenraghuvanshi@gmail.com>
2022-01-09 03:41:07 +0530

    Default Portfolio code: https://github.com/zinedineBenkhider/portfolio

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -3,10 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as AOS from 'aos';
 
 if (environment.production) {
   enableProdMode();
 }
 
+AOS.init();
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  
