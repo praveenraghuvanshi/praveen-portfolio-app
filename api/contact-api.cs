b524f21ae070a9746761b91a1c9b3b2d117b0bd2
praveen raghuvanshi <praveenraghuvanshi@gmail.com>
2022-01-10 02:37:16 +0530

    Update contact-api.cs

--- api/contact-api.cs
+++ api/contact-api.cs
@@ -7,8 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Portfolio.Function
 {
