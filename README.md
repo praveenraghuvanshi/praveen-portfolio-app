dcde7bb8f72e527df61ba0e7488ea4ffb8e17150
praveen raghuvanshi <praveenraghuvanshi@gmail.com>
2022-01-09 11:38:04 +0530

    Renamed angular project to portfolio-app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# praveen-portfolio-app
+# Praveen's Portfolio
+
+URL: https://calm-mud-069841900.azurestaticapps.net/
+
+## References
+
+- [GitHub - zinedineBenkhider/portfolio](https://github.com/zinedineBenkhider/portfolio)
--- portfolio-app/src/app/home/home.component.ts
+++ portfolio-app/src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     var options = {
-      strings: ['','Full-Stack', 'WEB','Mobile'],
+      strings: ['','Cloud-Native', 'Microservices', '.Net(C#)', 'AI/ML', 'Web'],
       typeSpeed: 120,
       backSpeed: 100,
       loop: true,
